8262f1450c73290112553957958e13d6dbd00875
Shijie Wu <wushijie5@gmail.com>
2017-05-05 17:26:46 -0400

    fix multigpu graph; add single thread with multigpu (working but same as single gpu)

--- multigpu-parallel-imagenet-blur-val.py
+++ multigpu-parallel-imagenet-blur-val.py
@@ -24,7 +24,6 @@ slim = tf.contrib.slim
 
 height, width = 299, 299
 num_classes = 1001
-c1, c5 = 0, 0
 lines = np.loadtxt('imagenet/val.txt', str, delimiter='\n')
 nb_gpus = len(sys.argv[1].split(','))
 
@@ -49,7 +48,6 @@ from multiprocessing.queues import SimpleQueue
 from math import floor
 fetch_test = False
 def fetch_img(prc_i, num_t, img_q):
-    print('Thread %d start!!!' % prc_i)
     lines = np.loadtxt('imagenet/val.txt', str, delimiter='\n')
     steps = int(floor(lines.size / num_t))
     start = steps * prc_i
@@ -58,20 +56,14 @@ def fetch_img(prc_i, num_t, img_q):
     if prc_i == num_t - 1:
         end = lines.size
     for i in range(start, end):
-        print('Thread %d start loop' % prc_i)
         imname, label = lines[i].split(' ')
         label = int(label) + 1
-        print('Thread %d load image' % prc_i)
         im = Image.open('imagenet/ILSVRC2012_img_val/' + imname).convert('RGB')
-        print('Thread %d filter image' % prc_i)
         im = np.array(im.filter(flt))
-        print('Thread %d proprocess image' % prc_i)
         processed_image = imresize(im, (width, height))
         processed_image = (processed_image.astype(np.float32) / 256 - 0.5) * 2
         processed_images = np.expand_dims(processed_image, axis=0)
-        print('Thread %d try to enqueue' % prc_i)
         img_q.put([processed_images, label])
-        print('Thread %d successfully enqueue' % prc_i)
 
 # img_q = Queue(maxsize=1024)
 img_q = SimpleQueue()
@@ -85,15 +77,18 @@ for i in range(num_t):
 
 def eval(rank, img_q, sess, op, img_tlt):
     img_cnt = 0
+    c1, c5 = 0, 0
     while img_cnt < img_tlt:
-        print('wait for image')
+        print('thread %d wait for image' % rank)
         processed_images, label = img_q.get()
-        print('got image')
+        print('thread %d got image' % rank)
+        print(processed_images.shape, op)
         if fetch_test:
             img_cnt += 1
             sys.stdout.write('\r{:07d}/{:07d}'.format(img_cnt, img_tlt))
             sys.stdout.flush()
             continue
+        print('thread %d feed' % rank)
         pred, prob = sess.run([op.pred, op.prob], feed_dict={
                 op.input: processed_images
             })
@@ -110,25 +105,66 @@ def eval(rank, img_q, sess, op, img_tlt):
         print('gpu %d images: %d\ttop 1: %0.4f\ttop 5: %0.4f' % (rank, idx + 1, c1/(idx + 1), c5/(idx + 1)))
         img_cnt += 1
 
+def eval_multi(nb_gpus, img_q, sess, ops, img_tlt):
+    assert len(ops) == nb_gpus
+    img_cnt = 0
+    c1, c5 = 0, 0
+    end_while = False
+    while img_cnt < img_tlt:
+        input = {}
+        target = []
+        output = []
+        for op in ops:
+            if img_cnt >= img_tlt:
+                end_while = True
+                break
+            processed_images, label = img_q.get()
+            input[op.input] = processed_images
+            output.append((op.pred, op.prob))
+            target.append(label)
+            img_cnt += 1
+        if end_while:
+            break
+        if fetch_test:
+            img_cnt += 1
+            sys.stdout.write('\r{:07d}/{:07d}'.format(img_cnt, img_tlt))
+            sys.stdout.flush()
+            continue
+        out = sess.run(output, feed_dict=input)
+        for (_, prob), label in zip(out, target):
+            prob = prob[0, 0:]
+            sorted_inds = [i[0] for i in sorted(enumerate(-prob), key=lambda x:x[1])]
+
+            top1 = sorted_inds[0]
+            top5 = sorted_inds[0:5]
+            if label == top1:
+                c1 += 1
+            if label in top5:
+                c5 += 1
+            idx = img_cnt
+            print('images: %d\ttop 1: %0.4f\ttop 5: %0.4f' % (idx + 1, c1/(idx + 1), c5/(idx + 1)))
+
 with tf.Graph().as_default():
-    config = tf.ConfigProto()
-    config.gpu_options.allow_growth = True
-    # config.log_device_placement = True
-    sess = tf.Session(config=config)
     subgraph = []
     for gpu in range(nb_gpus):
         print('loading graph on /gpu:%d' % gpu)
-        eval_input = tf.placeholder(tf.float32, [1, height, width, 3])
-        with tf.device('/gpu:%d' % gpu):
-            with tf.name_scope('tower-%d' % gpu):
+        with tf.name_scope('tower-%d' % gpu):
+            eval_input = tf.placeholder(tf.float32, [1, height, width, 3])
+            with tf.device('/gpu:%d' % gpu):
                 with slim.arg_scope(inception_arg_scope()):
                     resue = None if gpu == 0 else True
                     logits, _ = inception_v4(eval_input, num_classes, reuse=resue,
                                              scope='InceptionV4',
                                              is_training=False)
-                    predictions = tf.argmax(logits, 1)
-                probabilities = tf.nn.softmax(logits)
-                subgraph.append(OP(eval_input, predictions, probabilities))
+            predictions = tf.argmax(logits, 1)
+            probabilities = tf.nn.softmax(logits)
+            subgraph.append(OP(eval_input, predictions, probabilities))
+    
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+    # config.log_device_placement = True
+    # config.allow_soft_placement = True
+    sess = tf.Session(config=config)
 
     init_fn = slim.assign_from_checkpoint_fn(
         'checkpoints/inception_v4.ckpt',
@@ -137,9 +173,11 @@ with tf.Graph().as_default():
 
     print('finish building graph')
     img_tlt = lines.size
+    # eval_multi(nb_gpus, img_q, sess, subgraph, img_tlt)
     prc_g = []
     for gpu in range(nb_gpus):
         img_per_gpu = img_tlt - img_tlt / nb_gpus if i == 0 else img_tlt / nb_gpus
+        # eval(gpu, img_q, sess, subgraph[gpu], img_per_gpu)
         prc_g.append(Process(target=eval, args=(gpu, img_q, sess, subgraph[gpu], img_per_gpu)))
         prc_g[-1].daemon=True
         prc_g[-1].start()
